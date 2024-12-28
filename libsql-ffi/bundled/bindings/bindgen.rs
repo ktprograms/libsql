c1bd3a2c79a69de49526c8337e923849d72a1f37
kt programs <ktprograms@gmail.com>
2024-12-28 14:45:10 +0800

    libsql-ffi: build sqlite3mc_shell

--- libsql-ffi/bundled/bindings/bindgen.rs
+++ libsql-ffi/bundled/bindings/bindgen.rs
@@ -23,7 +23,6 @@ extern "C" {
     ) -> ::std::os::raw::c_int;
 }
 
-pub const __GNUC_VA_LIST: i32 = 1;
 pub const SQLITE_VERSION: &[u8; 7] = b"3.45.1\0";
 pub const SQLITE_VERSION_NUMBER: i32 = 3045001;
 pub const SQLITE_SOURCE_ID: &[u8; 85] =
@@ -502,8 +501,8 @@ pub const FTS5_TOKENIZE_DOCUMENT: i32 = 4;
 pub const FTS5_TOKENIZE_AUX: i32 = 8;
 pub const FTS5_TOKEN_COLOCATED: i32 = 1;
 pub const WAL_SAVEPOINT_NDATA: i32 = 4;
-pub type va_list = __builtin_va_list;
 pub type __gnuc_va_list = __builtin_va_list;
+pub type va_list = __builtin_va_list;
 extern "C" {
     pub static sqlite3_version: [::std::os::raw::c_char; 0usize];
 }
